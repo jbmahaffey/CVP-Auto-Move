eb36fc5e4f3e7f1580b25a710a3b00f40eb4742d
jbmahaffey <jbmahaffey@gmail.com>
2021-08-12 14:45:48 -0700

    Update Auto-move.py

--- Auto-move.py
+++ Auto-move.py
@@ -14,9 +14,9 @@ def Main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--cvp', default='192.168.101.26', help='CVP Server IP')
     parser.add_argument('--username', default='cvpadmin', help='CVP username')
-    parser.add_argument('--password', default='', help='CVP password')
+    parser.add_argument('--password', default='$3cr3t$3cr3t', help='CVP password')
     parser.add_argument('--logging', default='', help='Logging levels info, error, or debug')
-    parser.add_argument('--devlist', default='devices.csv', help='YAML file with list of approved devices.')
+    parser.add_argument('--devlist', default='devices.csv', help='YAML/CSV file with list of approved devices.')
     args = parser.parse_args()
 
     # Only enable logging when necessary
