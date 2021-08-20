68c2f9e8d466a26f83454f3457d72de1c678d6a6
jbmahaffey <jbmahaffey@gmail.com>
2021-08-19 17:34:34 -0700

    Update Auto-move.py

--- Auto-move.py
+++ Auto-move.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import requests
 import yaml
 from cvprac.cvp_client import CvpClient
 import argparse
@@ -13,7 +14,11 @@ def Main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--cvp', default='192.168.101.26', help='CVP Server IP')
     parser.add_argument('--username', default='cvpadmin', help='CVP username')
+<<<<<<< Updated upstream
     parser.add_argument('--password', default='', help='CVP password')
+=======
+    parser.add_argument('--password', default='$3cr3t$3cr3t', help='Cloudvision password')
+>>>>>>> Stashed changes
     parser.add_argument('--logging', default='', help='Logging levels info, error, or debug')
     parser.add_argument('--devlist', default='devices.yml', help='YAML file with list of approved devices.')
     args = parser.parse_args()
@@ -53,40 +58,64 @@ def Main():
             device = clnt.api.get_device_by_mac(dev['mac'])
             try:
                 tsk = clnt.api.deploy_device(device=device, container=dev['container'])
-                tasks.append(tsk['data']['taskIds'])
-                con = Configlet(clnt, dev)
-                if con != None:
+                Execute(clnt, tsk['data']['taskIds'])
+                con = Configlet(clnt, dev, args.cvp, args.username, args.password)
+                if con != None and con != 'reconcile':
                     assign = AssignConfiglet(clnt, dev, con)
-                    if assign['data']['taskIds'] not in tasks:
-                        tasks.append(assign['data']['taskIds'])
-                    else:
-                        logging.info('Task already exist.')
+                    Execute(clnt, assign['data']['taskIds'])
+                elif con == 'reconcile':
+                    cfglets = Containercfg(clnt, dev)
+                    Execute(clnt, cfglets['data']['taskIds'])
                 else:
                     logging.info('Configlet already exist.')
             except:
                 logging.error('Unable to deploy device.')
         else:
             logging.info('device ' + str(undef) + ' not approved for deployment or already provisioned.')
-    
+
     # Run the task using the Execute function
     Execute(clnt, tasks)
 
 
 # Function to create configlet for management
-def Configlet(clnt, data):
+def Configlet(clnt, data, cvp, user, password):
     l = []
+<<<<<<< Updated upstream
     config = clnt.api.get_configlets(start=0, end=0)
+=======
+    try:
+        config = clnt.api.get_configlets(start=0, end=0)
+        ztp = clnt.api.get_device_by_mac(data['mac'])
+    except:
+        logging.error('Unable to get list of configlets.')
+
+>>>>>>> Stashed changes
     for configlet in config['data']:
         l.append(configlet['name'])
     
     if data['hostname'] + str('_mgmt') in l:
         logging.info('Configlet ' + str(data['hostname'] + '_mgmt') + ' already exist')
-    else:
+    elif ztp['ztpMode'] == 'true':
         try:
             cfglt = clnt.api.add_configlet(name=data['hostname'] + str('_mgmt'), config='hostname ' + str(data['hostname']) + '\ninterface management1\nip address ' + str(data['ip']) + '/24\nno shut\nip route 0.0.0.0/0 ' + str(data['mgmtgateway']) + '\ndaemon TerminAttr\nexec /usr/bin/TerminAttr -ingestgrpcurl=192.168.101.26:9910 -cvcompression=gzip -ingestauth=key,arista -smashexcludes=ale,flexCounter,hardware,kni,pulse,strata -ingestexclude=/Sysdb/cell/1/agent,/Sysdb/cell/2/agent -ingestvrf=default -taillogs\nno shut')
             return cfglt
         except:
             logging.error('Unable to create configlet ' + str(data['hostname'] + '_mgmt'))
+    else:
+        try:
+            container = clnt.api.get_container_by_name(name=data['container'])
+            ckey = container['key']
+            login = 'https://{server}/cvpservice/login/authenticate.do'.format(server=cvp)
+            resp = requests.post(login, headers={'content-type': 'application/json'}, json={'userId': user, 'password': password}, verify=False)
+            jresp = resp.json()
+            token = jresp['cookie']['Value']
+            url = 'https://{server}/cvpservice/provisioning/containerLevelReconcile.do?containerId={container}&reconcileAll=false'.format(server=cvp, container=ckey)
+            response = requests.get(url, auth=(user, password), headers={'Cookie': 'access_token=' + str(token)}, verify=False)
+            if response.status_code == 200:
+                reconcile = 'reconcile'
+            return reconcile
+        except:
+            logging.error('Unable to reconcile container.')
 
 
 # function to assign configlet to new device
@@ -97,11 +126,21 @@ def AssignConfiglet(clnt, dev, con):
     return task
 
 
+def Containercfg(clnt, data):
+    cfglets = clnt.api.get_configlets_by_device_id(data['mac'])
+    cfglist = []
+    for configlet in cfglets:
+        cfglist.append({'name': configlet['name'], 'key': configlet['key']})
+    device = clnt.api.get_device_by_mac(data['mac'])
+    task = clnt.api.apply_configlets_to_device(app_name='container_configlet', dev=device, new_configlets=cfglist)
+    return task
+
+
 # Function to run task if they are for the devices we provisioned
 def Execute(clnt, tasks):
     for task in tasks:
         try:
-            clnt.api.execute_task(task_id=task[0])
+            clnt.api.execute_task(task_id=task)
         except:
             logging.info('Task ID ' + str(task) + ' is ' + ' failed to execute.')
     
